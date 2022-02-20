d134e5816350baa7b0616239699238edac5d43cd
doctok501 <doctok501@gmail.com>
2022-02-20 19:58:35 +0900

    게시물 작성시 작성날짜 저장

---
+++ src/com/KIK/java/Util.java
@@ -0,0 +1,15 @@
+package com.KIK.java;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+	public static String getNowDateTimeStr() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Date time = new Date();
+		
+		return format.format(time);
+		
+	}
+}
